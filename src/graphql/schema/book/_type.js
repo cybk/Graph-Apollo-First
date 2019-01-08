9f0bccee0465971c7e6958b00cafe606d59e31f6
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-01-08 17:42:09 -0600

    Adding changes for ebooks section

---
+++ src/graphql/schema/book/_type.js
@@ -0,0 +1,12 @@
+const Book = `
+    type Book {
+        title: String!
+        autor: String!
+    }
+`;
+
+export const types =  () => [Book];
+
+export const typeResolvers = {
+    
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 
+// import GraphQl - Apollo
+import Apollo from './graphql'
+
+import config from './config';
+
 const app = express();
 
 function setPort(port = 500) {
@@ -8,20 +13,22 @@ function setPort(port = 500) {
 }
 
 function listen() {
-    const port = app.get('port') || 500;
+    const port = app.get('port') || config.port;
     app.listen(port, () => {
         console.log(`The server is running and listening at http://localhost:${port}`);
     });
 }
 
 app.use(cors({
-    origin: '*',
+    origin: config.corsDomain,
     optionsSuccessStatus: 200
 }));
 
 app.get('/api/status', (req, res) => {
     res.send({status: 'ok'});
 });
+
+Apollo(app);
 
 export default {
     getApp: () => app,
